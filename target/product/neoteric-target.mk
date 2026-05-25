6a6bd2f540dc9b1d65565da013fd4c48ebc98369
ShadeDark6 <nizamfa6@gmail.com>
2026-05-25 16:49:53 +0000

    neoteric: Drop CertifiedPropsOverlay

--- target/product/neoteric-target.mk
+++ target/product/neoteric-target.mk
@@ -100,9 +100,6 @@ $(call inherit-product, vendor/gms/products/gms.mk)
 # Pixel goodies
 $(call inherit-product, vendor/neoteric_prebuilt/pixel/packages.mk)
 
-# GMS Spoof
-$(call inherit-product, vendor/gms_spoof/config.mk)
-
 PRODUCT_PRODUCT_PROPERTIES += \
     remote_provisioning.enable_rkpd=true \
     remote_provisioning.hostname=remoteprovisioning.googleapis.com
